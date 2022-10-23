b5a89a27b08c6513e5b511b45ff39cfc69338db3
andre_rodrigues <lrandre601@gmail.com>
2022-10-23 17:33:02 -0300

    Insert User

--- src/main/java/com/andrerodrigues/web/resources/UserResource.java
+++ src/main/java/com/andrerodrigues/web/resources/UserResource.java
@@ -6,8 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import java.net.URI;
 
 import com.andrerodrigues.web.entities.User;
 import com.andrerodrigues.web.services.UserService;
@@ -32,5 +36,18 @@ public class UserResource {
 	public ResponseEntity<User> findById(@PathVariable Long id) {
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
+	}
+
+	// POST
+	// Annotation @RequestBody faz o Json ser convertido para objeto java (Deserialização)
+	// Quando inserimos recurso no BD, devemos obter resposta HTTP 201
+	// Para isso, devemos retornar "ResponseEntity.created(URI)"
+	// URI conterá o endereço/localização do novo recurso criado/inserido
+	@PostMapping
+	public ResponseEntity<User> insert(@RequestBody User obj) {
+		obj = service.insert(obj);
+		// O endereço do recurso inserido será o endereço atual /users + o {id} do recurso inserido 
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
 	}
 }
